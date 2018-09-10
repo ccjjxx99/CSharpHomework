6d0063c81408337c4d71e26379af2194e72511e6
常佳鑫 <ccjjxx99@qq.com>
2018-09-10 23:03:50 +0800

    Finish homework1

---
+++ Homework1/Program1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("Please input two numbers:");
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("The product of " + a + " and " + b + " is " + (a * b));
+        }
+    }
+}
