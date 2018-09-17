bad05e19702f3d720ee750f24cd67d0d339d4905
常佳鑫 <ccjjxx99@qq.com>
2018-09-18 00:20:40 +0800

    Finished

---
+++ homework2/Program1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.Write("Please input the range from:");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("to:");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("The prime numbers from " + a + " to " + b + " are: ");
+            for (int i = a; i < b; i++)
+            {
+                if (i <= 1) i = 2;
+                bool flag = true;
+                for (int j = 2; j < i; j++)
+                {
+                    if (i % j == 0) flag = false;
+                }
+                if (flag)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+    }
+}
---
+++ homework2/Program3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The prime numbers from 2 to 100 are:");
+            int[] a = new int[99];
+            for (int i = 0; i < 99; i++)
+            {
+                a[i] = i + 2;
+            }
+            for (int i = 2; i < 100; i++)
+            {
+                for (int j = 2; j <= 100; j++)
+                {
+                    if (j == i) continue;
+                    if (j % i == 0) a[j - 2] = 0;
+                }
+            }
+            for (int i = 0; i < 99; i++)
+            {
+                if (a[i] != 0) Console.Write(a[i] + " ");
+            }
+        }
+    }
+}
