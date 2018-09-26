178813d407c402697dac01dcd045828ad8202297
常佳鑫 <ccjjxx99@qq.com>
2018-09-26 23:37:02 +0800

    Fix naming rules (change homework2 to Homework2)

---
+++ Homework2/Program2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input a set of numbers (separate by spaces and end by enter):");
+            String s = Console.ReadLine();
+            String[] sA = s.Split(' ');
+            int[] a = new int[sA.Length];
+            for (int i = 0; i < sA.Length; i++)
+            {
+                a[i] = int.Parse(sA[i]);
+            }
+            int min = a[0];
+            int max = a[0];
+            int sum = 0;
+            double avr;
+            for (int i = 0; i < sA.Length; i++)
+            {
+                if (a[i] < min) min = a[i];
+                if (a[i] > max) max = a[i];
+                sum += a[i];
+            }
+            avr = (double)sum / a.Length;
+            Console.WriteLine("The minimum is " + min);
+            Console.WriteLine("The maximum is " + max);
+            Console.WriteLine("The sum is " + sum);
+            Console.WriteLine("The average is " + avr);
+        }
+    }
+}
